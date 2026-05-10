e07a1e98be665ef23600e029554fadfb430f9a0f
Shriram Lahane <172600005+shriram7057@users.noreply.github.com>
2026-05-10 11:16:06 +0530

    Add files via upload

---
+++ LeetCode-POTD-Solutions/Maximum Number of Jumps to Reach the Last Index.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumJumps(int[] nums, int target) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+
+        // -1 means unreachable
+        for (int i = 0; i < n; i++) {
+            dp[i] = -1;
+        }
+
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                long diff = (long) nums[i] - nums[j];
+
+                if (diff >= -target && diff <= target && dp[j] != -1) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
